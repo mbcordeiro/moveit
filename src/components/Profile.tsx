191723326633c07d0b64141600f018b7d414ea1a
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-02-26 22:27:42 -0300

    Initial component profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return (
+        <div>
+            <img src="https://avatars.githubusercontent.com/u/55818513?s=460&u=20b00b19981de2d48883d8ad159db9db2ad06f4a&v=4" alt="Matheus Cordeiro"/>
+            <div>
+                <strong>Matheus Cordeiro</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
