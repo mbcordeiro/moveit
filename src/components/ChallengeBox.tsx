6ab47e86f9f7de20bd3423b006a2edcbdff0f254
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-05-11 13:14:35 -0300

    Random challenge and experience bar dynamic and completed chalenges

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -3,21 +3,28 @@ import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/ChallengeBox.module.css";
 
 export function ChallengeBox() {
-  const contextData = useContext(ChallengesContext);
-  console.log(contextData )
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+
   return (
     <div className={styles.challengeContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
           <main>
-            <img src="/icons/body.svg" />
+            {activeChallenge.type === "body" ? (
+              <img src="/icons/body.svg" />
+            ) : (
+              <img src="/icons/eye.svg" />
+            )}
             <strong>Novo desafio</strong>
-            <p>Descrição</p>
+            <p>{activeChallenge.description}</p>
           </main>
           <footer>
-            <button type="button" className={styles.challengeFailedButton}>
+            <button
+              type="button"
+              className={styles.challengeFailedButton}
+              onClick={resetChallenge}
+            >
               Falhei
             </button>
             <button type="button" className={styles.challengeSucceededButton}>
