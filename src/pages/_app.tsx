2fd9231dc96f9e713c0e2d06da39bd4b82116006
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-05-11 12:50:03 -0300

    Challenge context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import Head from "next/head";
+import {
+  ChallengesContext,
+  ChallengesProvider,
+} from "../contexts/ChallengesContext";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  );
 }
 
 export default MyApp;
